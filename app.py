ab69e077c3832ee04424c431f9cce277aa00aaee
laulpz <51860984+laulpz@users.noreply.github.com>
2025-07-31 20:47:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -284,7 +284,7 @@ else:
 
 st.sidebar.markdown("---")
 from db_manager import init_db, reset_db, guardar_asignaciones, cargar_asignaciones, confirmar_asignaciones
-    reset_db()
+reset_db()
     init_db()
     st.sidebar.success("✅ Base de datos reseteada y reestructurada correctamente.")
     for key in list(st.session_state.keys()):
