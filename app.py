b53dccfd96b117b4dfa8fb729065d3ca3232f3d1
laulpz <51860984+laulpz@users.noreply.github.com>
2025-08-12 16:50:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from db_manager import (
 )
 
 st.set_page_config(page_title="Inicio", page_icon="🏥", layout="wide", initial_sidebar_state="expanded",
-                   menu_items={'Get Help': 'https://www.extremelycoolapp.com/help', 'Report a bug': "https://www.extremelycoolapp.com/bug", 'About': "# Esta es una aplicación para el TFM de Laura P"})
+                   menu_items={'About': "# Esta es una aplicación para el TFM de Laura P"})
 
 # === CONFIGURA TU FILE_ID DE GOOGLE DRIVE AQUÍ ===
 FILE_ID = "1zqAyIB1BLfCc2uH1v29r-clARHoh2o_s"
