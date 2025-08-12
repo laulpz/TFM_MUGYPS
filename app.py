019ffa1b579baef0bde0644a8f37bc8ea98183fd
laulpz <51860984+laulpz@users.noreply.github.com>
2025-08-12 16:54:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,10 @@ from db_manager import (
     cargar_asignaciones, descargar_bd_desde_drive, subir_bd_a_drive
 )
 
-st.set_page_config(page_title="Inicio", page_icon="🏥", layout="wide", initial_sidebar_state="expanded",
-                   menu_items={'About': "# Esta es una aplicación para el TFM de Laura P"})
+st.set_page_config(page_title="Inicio", page_icon="🏥", layout="wide", initial_sidebar_state="expanded",)
 
 # === CONFIGURA TU FILE_ID DE GOOGLE DRIVE AQUÍ ===
 FILE_ID = "1zqAyIB1BLfCc2uH1v29r-clARHoh2o_s"
-
-st.markdown("""
-<style>
-[data-testid="stSidebarNavItems"] {
-    gap: 0.5rem;
-    padding: 0.5rem !important;
-}
-</style>
-""", unsafe_allow_html=True)
 
 # Sincronizar base de datos
 descargar_bd_desde_drive(FILE_ID)
